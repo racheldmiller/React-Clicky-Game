82e43e5ef07486a920bbf28e3393cca87e6f1a12
racheldmiller <rachelmiller415@gmail.com>
2019-08-31 13:16:43 -0700

    changed file extensions, still doesn't work

--- src/images.js
+++ src/images.js
@@ -1,13 +1,13 @@
 // Import all Spongebob images from src > assets > images
-import spongebob1 from "./assets/images/spongebob1.png";
-import spongebob2 from "./assets/images/spongebob2.png";
-import spongebob3 from "./assets/images/spongebob3.png";
-import spongebob4 from "./assets/images/spongebob4.png";
-import spongebob5 from "./assets/images/spongebob5.png";
-import spongebob6 from "./assets/images/spongebob6.png";
-import spongebob7 from "./assets/images/spongebob7.png";
-import spongebob8 from "./assets/images/spongebob8.png";
-import spongebob9 from "./assets/images/spongebob9.png";
+import spongebob1 from "./assets/images/spongebob1.jpg";
+import spongebob2 from "./assets/images/spongebob2.jpg";
+import spongebob3 from "./assets/images/spongebob3.jpg";
+import spongebob4 from "./assets/images/spongebob4.jpg";
+import spongebob5 from "./assets/images/spongebob5.jpg";
+import spongebob6 from "./assets/images/spongebob6.jpg";
+import spongebob7 from "./assets/images/spongebob7.jpg";
+import spongebob8 from "./assets/images/spongebob8.jpg";
+import spongebob9 from "./assets/images/spongebob9.jpg";
 
 const images = [
   spongebob1,
