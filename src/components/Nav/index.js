537176784413d12c2e08e17fe2880f3c4cb1d194
racheldmiller <rachelmiller415@gmail.com>
2019-08-31 22:32:20 -0700

    file cleanup

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./style.css";
 
 // -------------------------------- NAVBAR -------------------------------
-const Nav = props => (
-  <nav className="nav">
-    {/* Have a landing page link */}
-    <div className="game-brand">
-      <a href="/">CLICKY GAME!</a>
-    </div>
-    <div className="scores">
-      Current Score: {props.score} | Highest Score: {props.highscore}
-    </div>
-  </nav>
-);
+
+function Nav(props) {
+  return (
+    <nav className="nav">
+      <div className="game-brand">
+        <a href="/">CLICKY GAME!</a>
+      </div>
+      <div className="stats">
+        Current Score: {props.score} | Highest Score: {props.highscore}
+      </div>
+    </nav>
+  );
+}
 
 export default Nav;
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Wrapper = props => <div>{props.children}</div>;
+// const Wrapper = props => <div>{props.children}</div>;
+
+function Wrapper(props) {
+  return <main className="wrapper">{props.children}</main>;
+}
 
 export default Wrapper;
